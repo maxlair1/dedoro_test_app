fe7fcc582f97523f6561c0aae35d486af598252d
Max Lair <max@lairfamily.com>
2025-07-03 13:23:10 -0400

    Initial

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Pomodoro from "~/components/pomodoro";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Pomodoro />;
 }
